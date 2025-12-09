9e275c17539d244bb85910a97412da10863202f3
Darshanr <darshanr974151@gmail.com>
2025-12-09 15:03:21 +0530

    another comit

--- Month1_work_track/day0_mysql.sql
+++ Month1_work_track/day0_mysql.sql
@@ -9,4 +9,14 @@ from Student s
 left join Examinations e
 on s.student_id=e.student_id and s.subject_name=e.subject_name
 group by  s.student_id,s.subject_name
-order by s.student_id,s.subject_name
+order by s.student_id,s.subject_name;
+--
+with cte as
+(
+    select managerId
+    from Employee
+    group by managerId
+    having count(id) >=5
+)
+select name from Employee
+where id in (select * from cte);

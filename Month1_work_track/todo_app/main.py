d89151c7e870ee5fc90f2358571a2eecc6da4b81
Darshanr <darshanr974151@gmail.com>
2025-12-14 17:21:48 +0530

    DAY2 of my To-Do APP(backend)

--- Month1_work_track/todo_app/main.py
+++ Month1_work_track/todo_app/main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException# type: ignore
+
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")
+class TodoItem(BaseModel):
+    id:int
+    task:str
+    Completed:bool=False
+
+
+todos={}
+
+
+@app.get("/todos")
 def router():
-    return {"message": "hello Darshan"}
+    return list(todos.values())
+
+@app.post("/todos")
+def add_todo(todo : TodoItem):
+    if todo.id in todos:
+        raise HTTPException(status_code=400,detail="id already exists")
+    todos[todo.id]=todo
+    return {"meassage":"Todo added sucessfully"}

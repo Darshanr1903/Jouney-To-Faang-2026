6e5ac3a1d59b368fc9527335f1be06e1317b602b
Darshanr <darshanr974151@gmail.com>
2025-12-14 10:06:38 +0530

    day2

---
+++ Month1_work_track/mysql/day0.sql
@@ -0,0 +1,22 @@
+with Student as
+(
+    select s.student_id ,s.student_name,s1.subject_name
+    from Students s
+    cross join Subjects s1
+)
+select s.student_id,s.student_name,s.subject_name, count(e.subject_name) as attended_exams
+from Student s
+left join Examinations e
+on s.student_id=e.student_id and s.subject_name=e.subject_name
+group by  s.student_id,s.subject_name
+order by s.student_id,s.subject_name;
+--
+with cte as
+(
+    select managerId
+    from Employee
+    group by managerId
+    having count(id) >=5
+)
+select name from Employee
+where id in (select * from cte);

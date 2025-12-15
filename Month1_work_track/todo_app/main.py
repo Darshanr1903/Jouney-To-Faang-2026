842e96208573d8f628d873edd708f5d24e1f011a
Darshanr <darshanr974151@gmail.com>
2025-12-15 23:27:50 +0530

    todays work on my todo app (backend)

--- Month1_work_track/todo_app/main.py
+++ Month1_work_track/todo_app/main.py
@@ -24,3 +24,19 @@ def add_todo(todo : TodoItem):
         raise HTTPException(status_code=400,detail="id already exists")
     todos[todo.id]=todo
     return {"meassage":"Todo added sucessfully"}
+
+@app.put("/todos/{id}")
+def update_todo(id:int):
+    if id not in todos:
+        raise HTTPException(status_code=404,detail="NOT FOUND")
+    
+    todos[id].Completed=True
+        
+    return {"meassage":"Todo updated sucessfully"}
+
+@app.delete("/todos/{id}")
+def delete_task(id:int):
+    if id not in todos:
+        raise HTTPException(status_code=404,detail=f"no particular data with {id} exists")
+    del todos[id]
+    return {"message":"task deleted sucessfully"}

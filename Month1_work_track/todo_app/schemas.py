cfc2950a1800b8cf87fe22d87f297de159780ce2
Darshanr <darshanr974151@gmail.com>
2026-02-01 00:17:48 +0530

    pending updates of todo backend work

--- Month1_work_track/todo_app/schemas.py
+++ Month1_work_track/todo_app/schemas.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel, Field
 from typing import Optional
 
 class TodoItem(BaseModel):
+    # ... means field required
     id:int=Field(...,example=1,description="every task as unique ID")
     task:str=Field(...,example="Watch movies",description="this contains name of the task")
     completed:bool=Field(default=False,example=False,description="status of the task whether completed?")

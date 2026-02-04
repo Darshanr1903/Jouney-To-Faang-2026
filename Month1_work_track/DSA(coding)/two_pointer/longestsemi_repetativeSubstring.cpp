fd6233ec26a3f26ddb1fbf1fb26a92adee7636de
Darshanr <darshanr974151@gmail.com>
2026-02-05 01:12:55 +0530

    solved one more problem comenstating for 2nd feb

---
+++ Month1_work_track/DSA(coding)/two_pointer/longestsemi_repetativeSubstring.cpp
@@ -0,0 +1,74 @@
+// 2730. Find the Longest Semi-Repetitive Substring
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given a digit string s that consists of digits from 0 to 9.
+
+// A string is called semi-repetitive if there is at most one adjacent pair of the same digit. For example, "0010", "002020", "0123", "2002", and "54944" are semi-repetitive while the following are not: "00101022" (adjacent same digit pairs are 00 and 22), and "1101234883" (adjacent same digit pairs are 11 and 88).
+
+// Return the length of the longest semi-repetitive substring of s.
+
+ 
+
+// Example 1:
+
+// Input: s = "52233"
+
+// Output: 4
+
+// Explanation:
+
+// The longest semi-repetitive substring is "5223". Picking the whole string "52233" has two adjacent same digit pairs 22 and 33, but at most one is allowed.
+
+// Example 2:
+
+// Input: s = "5494"
+
+// Output: 4
+
+// Explanation:
+
+// s is a semi-repetitive string.
+
+// Example 3:
+
+// Input: s = "1111111"
+
+// Output: 2
+
+// Explanation:
+
+// The longest semi-repetitive substring is "11". Picking the substring "111" has two adjacent same digit pairs, but at most one is allowed.
+
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 50
+// '0' <= s[i] <= '9'
+#include<iostream>
+#include<string>
+using namespace std;
+class Solution {
+public:
+    int longestSemiRepetitiveSubstring(string s) {
+        int i=0,j=0;
+        int len=0;
+        int count=0;
+        while(j<s.length()){
+            if(j>0&&s[j]==s[j-1]){
+                count++;
+            }
+            while(count>1){
+                if(s[i]==s[i+1]) count--;
+                i++;
+            }
+            len=max(len,j-i+1);
+            j++;
+        }
+        return len;
+    }
+};

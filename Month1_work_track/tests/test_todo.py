e9821cc661652c0b38ae2412603d148e1a054227
Darshanr <darshanr974151@gmail.com>
2026-02-17 22:51:05 +0530

    17th feb todo backend work

--- Month1_work_track/tests/test_todo.py
+++ Month1_work_track/tests/test_todo.py
@@ -1,10 +1,5 @@
-import sys
-import os
 import pytest
 from fastapi.testclient import TestClient
-
-# Path setup
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from todo_app.main import app
 
@@ -30,3 +25,22 @@ def test_post_route():
         data = response.json()
         assert "id" in data
         assert data["content"] == "Watching Movie"
+
+
+def test_put_route():
+    id=1
+    flag=True
+    with TestClient(app) as client:
+        response=client.put(f"/todos/{id}",params={"status":flag})
+        assert response.status_code==200
+        data=response.json()
+        assert data["completed"]==flag
+
+def test_delete_route():
+    id=1
+    with TestClient(app) as client:
+        response=client.delete(f"/todos/{id}",params={"id":id})
+        assert response.status_code==200
+        data=response.json()
+        assert data["message"]==f"Task with {id} was deleted successfully"
+

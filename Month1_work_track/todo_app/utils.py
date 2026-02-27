a821e87fe9a09074350c6e18454bbda8d2d28a01
Darshanr <darshanr974151@gmail.com>
2026-02-28 00:22:34 +0530

    27th feb work on authentication system

---
+++ Month1_work_track/todo_app/utils.py
@@ -0,0 +1,12 @@
+from passlib.context import CryptContext
+
+pwd_context=CryptContext(schemes=["argon2","bycrpt"],deprecated="auto")
+
+
+def hashed_password(password:str)->str:
+    "this will be taking plain password and will be returning hash"
+    return pwd_context.hash(password)
+
+
+def verifying_password(plain_password:str,hash_password:str)->bool:
+    return pwd_context.verify(plain_password,hash_password)

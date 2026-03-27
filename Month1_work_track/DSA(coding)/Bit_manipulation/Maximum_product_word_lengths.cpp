b44f6dba4581819e9691da2a3d32b18ec2be0619
Darshanr <darshanr974151@gmail.com>
2026-03-28 01:19:12 +0530

    27th march DSA problems

---
+++ Month1_work_track/DSA(coding)/Bit_manipulation/Maximum_product_word_lengths.cpp
@@ -0,0 +1,56 @@
+// 318. Maximum Product of Word Lengths
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Given a string array words, return the maximum value of length(word[i]) * length(word[j]) where the two words do not share common letters. If no such two words exist, return 0.
+
+ 
+
+// Example 1:
+
+// Input: words = ["abcw","baz","foo","bar","xtfn","abcdef"]
+// Output: 16
+// Explanation: The two words can be "abcw", "xtfn".
+// Example 2:
+
+// Input: words = ["a","ab","abc","d","cd","bcd","abcd"]
+// Output: 4
+// Explanation: The two words can be "ab", "cd".
+// Example 3:
+
+// Input: words = ["a","aa","aaa","aaaa"]
+// Output: 0
+// Explanation: No such pair of words.
+ 
+
+// Constraints:
+
+// 2 <= words.length <= 1000
+// 1 <= words[i].length <= 1000
+// words[i] consists only of lowercase English letters.
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+        vector<int>ans;
+        for(int i=0;i<words.size();i++){
+            int x=0;
+            for(int j=0;j<words[i].length();j++){
+                x=x|(1<<(words[i][j]-'a'));
+            }
+            ans.push_back(x);
+        }
+        int max_length=0;
+        for(int i=0;i<ans.size();i++){
+            for(int j=i+1;j<ans.size();j++){
+                if(!(ans[i]&ans[j])){
+                    max_length=max(max_length,(int)words[i].length()*(int)words[j].length());
+                }
+            }
+        }
+        return max_length;
+    }
+};

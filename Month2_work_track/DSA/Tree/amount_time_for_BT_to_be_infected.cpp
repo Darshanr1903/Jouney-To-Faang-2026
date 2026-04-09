4d6a529b8e8b8b1e23cd72739b88279110d45bca
Darshanr <darshanr974151@gmail.com>
2026-04-09 22:56:07 +0530

    9th april DSA problem

---
+++ Month2_work_track/DSA/Tree/amount_time_for_BT_to_be_infected.cpp
@@ -0,0 +1,90 @@
+// 2385. Amount of Time for Binary Tree to Be Infected
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given the root of a binary tree with unique values, and an integer start. At minute 0, an infection starts from the node with value start.
+
+// Each minute, a node becomes infected if:
+
+// The node is currently uninfected.
+// The node is adjacent to an infected node.
+// Return the number of minutes needed for the entire tree to be infected.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [1,5,3,null,4,10,6,9,2], start = 3
+// Output: 4
+// Explanation: The following nodes are infected during:
+// - Minute 0: Node 3
+// - Minute 1: Nodes 1, 10 and 6
+// - Minute 2: Node 5
+// - Minute 3: Node 4
+// - Minute 4: Nodes 9 and 2
+// It takes 4 minutes for the whole tree to be infected so we return 4.
+// Example 2:
+
+
+// Input: root = [1], start = 1
+// Output: 0
+// Explanation: At minute 0, the only node in the tree is infected so we return 0.
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 105].
+// 1 <= Node.val <= 105
+// Each node has a unique value.
+// A node with a value of start exists in the tree.
+#include<iostream>
+#include<unordered_map>
+#include<unordered_set>
+using namespace std;
+// Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+unordered_map<TreeNode*,TreeNode*>mp;
+unordered_set<TreeNode*>s;
+int start;
+TreeNode* target;
+void parent_finder(TreeNode* root,TreeNode* parent_node){
+    if(root==NULL) return;
+    if(root->val==start) target=root;
+    mp[root]=parent_node;
+    parent_finder(root->left,root);
+    parent_finder(root->right,root);
+
+}
+int max_time=0;
+void dfs(TreeNode* root,int time){
+    if(root==NULL||s.find(root)!=s.end()){
+        max_time=max(max_time,time-1);
+        return;
+    }
+    s.insert(root);
+    dfs(root->left,time+1);
+    dfs(root->right,time+1);
+    dfs(mp[root],time+1);
+}
+    int amountOfTime(TreeNode* root, int start) {
+        this->start=start;
+        parent_finder(root,NULL);
+        dfs(target,0);
+        return max_time;
+        
+    }
+};
